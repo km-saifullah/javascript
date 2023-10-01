f930e5a97e7b4ecf05a2e4fbe61505e3f8b92186
km-saifullah <kmsaifullah16@gmail.com>
2023-10-01 23:53:13 +0600

    data type in javascript

---
+++ 01-basic/02-data-type.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all JS code as newer version
+/*
+alert("Hello");
+throw an error in terminal
+Because it will work with document in the browser
+*/
+
+let name = "Monmoy";
+let age = 25;
+let isLoggedIn = false;
+let state;
+
+console.log(typeof state);
+console.log(typeof name); // string
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
