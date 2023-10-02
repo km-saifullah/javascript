6467378dc3b720d34351eed23510b57e413bbbfd
km-saifullah <kmsaifullah16@gmail.com>
2023-10-02 11:31:43 +0600

    primitive and reference data type

---
+++ 01-basic/05-primitive-nonprimitive.js
@@ -0,0 +1,29 @@
+// Primitive Data types
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+const id = Symbol("123");
+const anotherId = Symbol("123");
+// console.log(id, anotherId);
+// console.log(id === anotherId);
+
+// const bigNum = 2345087476983565723462378n;
+// console.log(bigNum);
+
+// Reference Data Type
+const heroes = ["Superman", "Spiderman", "Ironman"];
+
+const car = {
+  type: "off-roading",
+  mileage: "34km",
+};
+
+const myFun = function () {
+  console.log("Hello World");
+};
+myFun();
+
